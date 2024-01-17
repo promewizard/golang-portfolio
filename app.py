5fe819b895abb4e6436026620e413b9a1ca9a043
promewizard <prometheus0730@outlook.com>
2024-01-18 00:34:15 +0530

    Refactored and completed about me section

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ work_experience = [
         'title': 'Cloud Computing Intern',
         'company': 'Whizlabs',
         'location': 'Remote',
-        'start_date': '2023-04-01',
+        'start_date': 'April 2023',
         'end_date': '',
         'description': "Effortlessly incorporated leading cloud services such as GKE, GCP Vertex AI, and various other cloud solutions into laboratory environments, enhancing user engagement through practical exposure to real cloud-based scenarios.\n" 
                         "Created CI/CD pipeline to build, test, and deploy internal tools on Google Cloud serverless platforms.\n"
@@ -16,17 +16,32 @@ work_experience = [
     },
 ]
 
-education = [
-    {}
+education_details = [
+    {
+        'institution': "Shri Mata Vaishno Devi University",
+        'start-year' : "2020",
+        'degree': "Bachelor of Technology",
+        'major': "Electronics and Communication Engineering",
+        'end-year' : "",
+    }
 ]
 
-skills = [
-    {}
+skills = ["Python", "C++", "Java", "HTML", "CSS", "JavaScript", "React", "Node.js", "Express.js", "MongoDB", "SQL", "Git", "Linux", "Docker", "Kubernetes", "AWS", "GCP", "Azure", "Tensorflow", "PyTorch"
 ]
 
 @app.route('/')
 def index():
-    return render_template('home.html', work_experience=work_experience)
+    return render_template('home.html', work_experience=work_experience
+                           , education_details=education_details
+                           ,skills=skills)
+
+@app.route('/certificates')
+def certificates():
+    return render_template('certificates.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
